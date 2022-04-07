a31d796238346186caa17c19affce115e93151bd
prasannamadusanka <wwprasannamadusanka@gmail.com>
2022-04-07 13:06:43 +0530

    unstage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import ClickClass from './components/CllickClass';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import StyleSheet from './components/StyleSheet';
-import Styles from './components/myStyle.module.css';
-import Form from './components/Form';
+
 
 function App() {
   return (
     <div className="App">
-      <Form></Form>
+      
        </div>
   );
 }
